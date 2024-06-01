f3efa87b6ad97e217805583502352921c3884558
Suresh Kumar <sureshkumar9129985584@gmail.com>
2024-06-01 17:52:40 +0530

    Files

---
+++ css/JavaScript.js
@@ -0,0 +1,30 @@
+// NavBar---------------------->
+
+// Map------------->
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("location");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Image slider ----------------->
+
+
+
